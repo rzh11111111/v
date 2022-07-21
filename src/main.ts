da2d83640bf7bfb3719041283e5b3bb7909f848c
rzh11111111 <294346639@qq.com>
2022-07-21 22:02:47 +0800

    useMouse

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
  import { createApp } from 'vue'
  import App from './App.vue'
  import store from './store'
- ​
+ import router from '@/router';
+
  // 创建vue实例
- const app = createApp(App)
- ​
- // 挂载pinia
- app.use(store)
- ​
+ const app = createApp(App);
+
+ app.use(router);
+ app.use(store);
  // 挂载实例
  app.mount('#app');
