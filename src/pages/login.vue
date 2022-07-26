0bca6f9e446b5ffd9138a974eda565db5f0ad20c
rzh11111111 <294346639@qq.com>
2022-07-26 16:48:11 +0800

    初始化

---
+++ src/pages/login.vue
@@ -0,0 +1,14 @@
+ <template>
+   <div>{{ userStore.name }}</div>
+<div>login</div>
+ </template>
+
+ <script lang="ts" setup>
+ import { useUserStore } from '@/store/user'
+
+ const userStore = useUserStore()
+ userStore.updateName('李四')
+ </script>
+<style lang="scss">
+ .root {}
+</style>
---
+++ src/pages/sass.vue
@@ -0,0 +1,9 @@
+
+<!--  <template>
+        <div class="root">
+                <h3>欢迎使用 scss</h3>
+            </div>
+     </template>
+ <style lang="scss">
+ .root {}
+</style> -->
