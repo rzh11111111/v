274cf274e14d0ac635b892eb5b3cc7f29fd5218e
rzh <294346639@qq.com>
2022-08-24 22:22:54 +0800

    fix：更改

--- src/pages/vueUse.vue
+++ src/pages/vueUse.vue
@@ -1,14 +1,11 @@
 <template>
     <h1> 测试 vueUse 的鼠标坐标 </h1>
     <h3>Mouse: {{ x }} x {{ y }}</h3>
-    <router-link to="/">点击跳转至首页2</router-link>
+    <router-link to="/">点击跳转至首页</router-link>
 </template>
 
 <script lang="ts" setup>
     import { useMouse } from '@vueuse/core';
 
-   
-            const { x, y } = useMouse();
-
-         
+    const { x, y } = useMouse();
 </script>
