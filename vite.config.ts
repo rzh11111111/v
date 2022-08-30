2f37dddc4c01478c7d517827202b5adbaa18ddd5
rzh <294346639@qq.com>
2022-08-30 20:39:54 +0800

    feat:自动引入ref等

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,13 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'src')
         }
     },
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            imports: ['vue', 'vue-router'],
+            dts: 'src/auto-imports.d.ts'
+        })
+    ],
     server: {
         port: 8080, //启动端口
         hmr: {
