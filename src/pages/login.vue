4a9d914e1a2ccba8a19add2089a0d7f59cc406e0
rzh11111111 <294346639@qq.com>
2022-10-03 19:42:39 +0800

    feat：页面间传参

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,8 +1,13 @@
 <template>
     <h2> 这里是登录页面 </h2>
     <br />
-    <router-link to="/">点击跳转至首页</router-link>
+    <router-link to="/">点击跳转至首页,query</router-link>
     <br /><br />
     <router-link to="/vueUse">点击跳转至vueUse页面1</router-link>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+    //没有r，是route
+import { useRoute } from 'vue-router';
+const route = useRoute()
+    console.log(route.query)
+</script>
