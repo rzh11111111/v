4b1433235e75b1fb48905888077b7614b5348d21
rzh11111111 <294346639@qq.com>
2022-10-09 13:44:41 +0800

    feat：全局异常监控

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import router from './router';
 // 创建vue实例
 const app = createApp(App);
 
+//vue3全局异常监控
+app.config.errorHandler = (err, vm, info) => {
+    console.log('[全局异常]',err,vm,info)
+}
 app.use(store);
 app.use(router);
 
